391af6e35c6087ba2b209268ca86da35fedbee37
J.victt <102590669+Victornunes21@users.noreply.github.com>
2023-12-22 08:12:29 -0300

    projertott

--- main.py
+++ main.py
@@ -1,95 +1,92 @@
-# Lista para armazenar os nomes dos titulares das contas
-titulares_contas = []
-# Lista para armazenar os números das contas
-numeros_contas = []
-# Lista para armazenar os saldos das contas
-saldos_contas = []
 
-# Função para criar uma conta bancária
-def criar_conta():
-    nome = input("Digite o nome do titular da conta: ")
-    numero_conta = input("Digite o número da conta: ")
-    saldo_inicial = float(input("Digite o saldo inicial da conta: "))
-    
-    # Adicionar os dados às listas correspondentes
-    titulares_contas.append(nome)
-    numeros_contas.append(numero_conta)
-    saldos_contas.append(saldo_inicial)
-    
-    print(f"Conta para {nome} criada com sucesso!")
+import tkinter as tk
+from tkinter import messagebox
+import random
 
-# Função para depositar dinheiro em uma conta
-def depositar():
-    numero_conta = input("Digite o número da conta em que deseja depositar: ")
-    valor_deposito = float(input("Digite o valor que deseja depositar: "))
-    
-    # Procurar a conta com o número especificado
-    for i in range(len(numeros_contas)):
-        if numeros_contas[i] == numero_conta:
-            saldos_contas[i] += valor_deposito
-            print(f"Depósito de R$ {valor_deposito:.2f} efetuado com sucesso na conta de {titulares_contas[i]}.")
-            return
-    
-    print("Conta não encontrada. O depósito não pôde ser efetuado.")
+class JogoForca:
+    def __init__(self, palavra):
+        self.palavra = palavra.upper()
+        self.palavra_escondida = ['_'] * len(palavra)
+        self.tentativas = 6
+        self.letras_usadas = set()
 
-def transferir():
-    conta_origem = input("Digite o número da conta de origem: ")
-    conta_destino = input("Digite o número da conta de destino: ")
-    valor_transferencia = float(input("Digite o valor da transferência: "))
-    
-    # Procurar a conta de origem
-    for i in range(len(numeros_contas)):
-        if numeros_contas[i] == conta_origem:
-            saldo_origem = saldos_contas[i]
-            
-            # Verificar se há saldo suficiente para a transferência
-            if saldo_origem >= valor_transferencia:
-                # Procurar a conta de destino
-                for j in range(len(numeros_contas)):
-                    if numeros_contas[j] == conta_destino:
-                        saldos_contas[i] -= valor_transferencia
-                        saldos_contas[j] += valor_transferencia
-                        print(f"Transferência de R$ {valor_transferencia:.2f} efetuada com sucesso de {titulares_contas[i]} para {titulares_contas[j]}.")
-                        return
-    print("Conta de origem, conta de destino ou saldo insuficiente. A transferência não pôde ser efetuada.")
-    
-# Função para verificar saldo
-def verificar_saldo():
-    numero_conta = input("Digite o número da conta para verificar o saldo: ")
-    
-    # Procurar a conta com o número especificado
-    for i in range(len(numeros_contas)):
-        if numeros_contas[i] == numero_conta:
-            print(f"Saldo da conta de {titulares_contas[i]}: R$ {saldos_contas[i]:.2f}")
-            return
-    
-    print("Conta não encontrada. O saldo não pôde ser verificado.")
+    def tentativa(self, letra):
+        letra = letra.upper()
+        if letra in self.letras_usadas:
+            return  # Se a letra já foi usada, não faz nada
+ 
+        self.letras_usadas.add(letra)
 
-# Exemplo de uso das funções
-while True:
-    print("Opções:")
-    print("1. Criar conta")
-    print("2. Depositar dinheiro")
-    print("3. Transferir dinheiro")
-    print("4. Verificar saldo")
-    print("5. Sair")
-    
-    opcao = input("Escolha uma opção: ")
-    
-    if opcao == '1':
-        criar_conta()
-    elif opcao == '2':
-        depositar()
-    elif opcao == '3':
-        transferir()
-    elif opcao == '4':
-        verificar_saldo()
-    elif opcao == '5':
-        break
-    else:
-        print("Opção inválida. Tente novamente.")
+        if letra in self.palavra:
+            for i, char in enumerate(self.palavra):
+                if char == letra:
+                    self.palavra_escondida[i] = letra
+        else:
+            self.tentativas -= 1
 
-# Mostrar todas as contas criadas
-print("Contas bancárias criadas:")
-for i in range(len(titulares_contas)):
-      print(f"Nome: {titulares_contas[i]}, Número da Conta: {numeros_contas[i]}, Saldo: R$ {saldos_contas[i]:.2f}")
+    def verificar_vitoria(self):
+        return '_' not in self.palavra_escondida
+
+    def verificar_derrota(self):
+        return self.tentativas <= 0
+
+    def obter_palavra_escondida(self):
+        return ' '.join(self.palavra_escondida)
+
+
+class JogoDaForcaApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Jogo da Forca")
+
+        self.palavras = ["PYTHON", "JAVASCRIPT", "JAVA", "HTML", "CSS", "RUBY", "PHP"]
+        self.jogo = JogoForca(random.choice(self.palavras))
+
+        self.frame_principal = tk.Frame(root)
+        self.frame_principal.pack(expand=True, padx=20, pady=20)
+
+        self.label_palavra = tk.Label(self.frame_principal, text=self.jogo.obter_palavra_escondida(), font=('Arial', 18))
+        self.label_palavra.pack()
+
+        self.label_tentativas = tk.Label(self.frame_principal, text=f"Tentativas restantes: {self.jogo.tentativas}")
+        self.label_tentativas.pack()
+
+        self.letra_entry = tk.Entry(self.frame_principal, width=5)
+        self.letra_entry.pack()
+
+        self.botao_tentar = tk.Button(self.frame_principal, text="Tentar", command=self.tentar_letra)
+        self.botao_tentar.pack()
+
+        self.label_letras_usadas = tk.Label(self.frame_principal, text="Letras usadas:")
+        self.label_letras_usadas.pack()
+
+        self.label_lista_letras = tk.Label(self.frame_principal, text="", font=('Arial', 12))
+        self.label_lista_letras.pack()
+
+    def tentar_letra(self):
+        letra = self.letra_entry.get()
+        self.jogo.tentativa(letra)
+        self.atualizar_interface()
+
+    def atualizar_interface(self):
+        self.label_palavra.config(text=self.jogo.obter_palavra_escondida())
+
+        if self.jogo.verificar_vitoria():
+            messagebox.showinfo("Parabéns!", "Boa guereiro(a)!")
+            self.root.quit()
+
+        if self.jogo.verificar_derrota():
+            messagebox.showinfo("Game Over", f"A palavra era '{self.jogo.palavra}'. Você perdeu!")
+            self.root.quit()
+
+        self.label_tentativas.config(text=f"Tentativas restantes: {self.jogo.tentativas}")
+        self.letra_entry.delete(0, tk.END)
+
+        letras_usadas = ', '.join(sorted(self.jogo.letras_usadas))
+        self.label_lista_letras.config(text=letras_usadas)
+
+
+root = tk.Tk()
+app = JogoDaForcaApp(root)
+root.geometry("300x250")  # Definindo um tamanho para a janela
+root.mainloop()
